edb6c0652978ff698b2440281058440f277861da
gabriel-alves-p <gabrielmpalves@gmail.com>
2022-04-19 22:36:19 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KqOQdEdOj9oeLnQ8JeKfT0UZGLFwT2SWK02rrXe2VvFlQtdLwTrIhuFRpnAHn2RnW6ZoGtfLy9vUvBtkTk7O9fK00uDmm9PgH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
